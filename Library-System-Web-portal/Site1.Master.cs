c839cd1305f08986b7d578db1f251f20d2b360d8
YaduAcharya <yaduacharya120@gmail.com>
2021-10-05 21:50:21 +0530

    In home page linking many buttons
    Like after sign in we should able to see author manage, publisher manage.
    Before login we should able to see sign up login link buttons.

--- Library-System-Web-portal/Site1.Master.cs
+++ Library-System-Web-portal/Site1.Master.cs
@@ -18,5 +18,59 @@ namespace Library_System_Web_portal
         {
 
         }
+
+        protected void AdminLogin(object sender, EventArgs e)
+        {
+            Response.Redirect("AdminPages/AdminLogin.aspx");
+        }
+
+        protected void AuthorManage(object sender, EventArgs e)
+        {
+            Response.Redirect("AdminPages/AdminAuthorMangement.aspx");
+        }
+
+        protected void PublisherManage(object sender, EventArgs e)
+        {
+            Response.Redirect("AdminPages/AdminPublisherMangement.aspx");
+        }
+
+        protected void BookInventory(object sender, EventArgs e)
+        {
+            Response.Redirect("AdminPages/AdminBookInventory.aspx");
+        }
+
+        protected void BookIssuing(object sender, EventArgs e)
+        {
+            Response.Redirect("AdminPages/AdminBookIssuing.aspx");
+        }
+
+        protected void MemberManagement(object sender, EventArgs e)
+        {
+            Response.Redirect("AdminPages/AdminMemberManagement.aspx");
+        }
+
+        //protected void ViewBooks(object sender, EventArgs e)
+        //{
+        //    Response.Redirect("AdminPages/AdminLogin.aspx");
+        //}
+
+        protected void UserLogin(object sender, EventArgs e)
+        {
+            Response.Redirect("UserPages/UserLogin.aspx");
+        }
+
+        protected void SignUp(object sender, EventArgs e)
+        {
+            Response.Redirect("UserPages/UserSignUp.aspx");
+        }
+
+       
+        //protected void HelloUser(object sender, EventArgs e)
+        //{
+        //    Response.Redirect("UserPages/AdminLogin.aspx");
+        //}
+
+        
+
     }
 }
