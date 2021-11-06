543947553558a385b3b421d69532e509ff071506
YaduAcharya <yaduacharya120@gmail.com>
2021-11-06 18:18:01 +0530

    removed aspx files outside folder
    Before because of webservice path if aspx files were inside folder web service were not hitting breakpoint when calling with ajax so now corrected it.

--- Library-System-Web-portal/AdminPages/AdminLogin.aspx.cs
+++ Library-System-Web-portal/AdminPages/AdminLogin.aspx.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Library_System_Web_portal.LibraryService;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Library_System_Web_portal_Service;
+using Library_System_Web_portal_Service.Library;
 
 namespace Library_System_Web_portal
 {
@@ -13,5 +17,36 @@ namespace Library_System_Web_portal
         {
 
         }
+
+        //user login
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            var memberID = txtAdminID.Text.Trim();
+            var passWord = txtPassword.Text.Trim();
+
+            Library_System_Web_portal_Service.Library.LoginInfo loginInfo = new Library_System_Web_portal_Service.Library.LoginInfo();
+
+            loginInfo = LogInInfo.GetCurrentAdminInfo(memberID, passWord);
+
+            if (loginInfo.Count > 0)
+            {
+                Session["FullName"] = loginInfo.FullName;
+                Session["MemberID"] = loginInfo.MemberID;
+                Session["Password"] = loginInfo.Password;
+                Session["Role"] = "Admin";
+
+                Response.Redirect("~/HomePage.aspx");
+            }
+            else if (loginInfo.Count == 0)
+            {
+                Response.Write("<script>alert('Invalid credentials');</script>");
+            }
+
+            Response.Write("<script>alert('loginInfo');</script>");
+
+
+        }
+
+
     }
 }
